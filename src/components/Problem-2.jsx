2abad593bf16887d9de0f29e2df515e7fc683ccc
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2023-12-20 02:42:14 +0600

    Problem 2 trying

--- src/components/Problem-2.jsx
+++ src/components/Problem-2.jsx
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import Swal from "sweetalert2/dist/sweetalert2.js";
+import "sweetalert2/src/sweetalert2.scss";
 
 const Problem2 = () => {
+  const [allContacts, setAllContacts] = useState([]);
+  const [usContacts, setUsContacts] = useState([]);
 
-    return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://contact.mediusware.com/api-doc/");
+        console.log(response);
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
 
-        <div className="container">
-            <div className="row justify-content-center mt-5">
-                <h4 className='text-center text-uppercase mb-5'>Problem-2</h4>
-                
-                <div className="d-flex justify-content-center gap-3">
-                <button className="btn btn-lg btn-outline-primary" type="button" >All Contacts</button>
-                <button className="btn btn-lg btn-outline-warning" type="button" >US Contacts</button>
-                </div>
-                
-            </div>
+        const data = await response.json();
+        console.log("Data: ", data);
+        const allContacts = data;
+        const usContacts = data.filter((contact) => contact.country === "US");
+
+        setAllContacts(allContacts);
+        setUsContacts(usContacts);
+      } catch (error) {
+        console.error("Error fetching contacts:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const handleOpenMdal = () => {
+    Swal.fire({
+      title: "Do you want to save the changes?",
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: "Save",
+      denyButtonText: `Don't save`,
+    }).then((result) => {
+      if (result.isConfirmed) {
+        Swal.fire("Saved!", "", "success");
+      } else if (result.isDenied) {
+        Swal.fire("Changes are not saved", "", "info");
+      }
+    });
+  };
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center mt-5">
+        <h4 className="text-center text-uppercase mb-5">Problem-2</h4>
+
+        <div className="d-flex justify-content-center gap-3">
+          <button
+            onClick={() => handleOpenMdal()}
+            className="btn btn-lg btn-outline-primary"
+            type="button"
+          >
+            All Contacts
+          </button>
+          <button
+            onClick={() => {
+              handleOpenMdal();
+            }}
+            className="btn btn-lg btn-outline-warning"
+            type="button"
+          >
+            US Contacts
+          </button>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Problem2;
